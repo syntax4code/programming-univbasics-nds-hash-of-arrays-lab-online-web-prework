feb126346d3029c78dcc9649dc3c556d8cc17fd9
Warren Harper II <warrenharper2@att.net>
2019-10-26 19:45:18 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -5,7 +5,7 @@ BASE_HOA = {
 }
 #expect(add_character(:chipmunks, "Dave")).to eq(["Alvin", "Simon", "Theodore", "Dave"])
 #expect(add_character(:third_earthers, "Snarf")).to eq(["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"])
-#
+#expect(add_character(:jetsons, "Astro")).to eq(["George", "Jane", "Judy", "Elroy", "Astro"])
 end
 def add_character(show, name)
   add_character = BASE_HOA[:third_earthers] << "Snarf"
