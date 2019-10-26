01a4320025507aadad8169f2065c21cb0e45928c
Warren Harper II <warrenharper2@att.net>
2019-10-26 19:25:32 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -7,6 +7,7 @@ BASE_HOA = {
 
 def add_character(show, name)
   BASE_HOA[:third_earthers]<<"Snarf"
-  :chipmunks<<"Dave"
+
+  BASE_HOA[:chipmunks]<<"Dave"
   # Should return the array of the 'show' argument
 end
