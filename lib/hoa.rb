659ec228072e17610adfd1b175b9817631e3e7b4
Warren Harper II <warrenharper2@att.net>
2019-10-26 19:47:10 +0000

    Done.

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -3,9 +3,13 @@ BASE_HOA = {
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
-
-
+#expect(add_character(:chipmunks, "Dave")).to eq(["Alvin", "Simon", "Theodore", "Dave"])
+#expect(add_character(:third_earthers, "Snarf")).to eq(["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"])
+#expect(add_character(:jetsons, "Astro")).to eq(["George", "Jane", "Judy", "Elroy", "Astro"])
+end
 def add_character(show, name)
-  # Write your implementation here
+  BASE_HOA[:chipmunks] << "Dave"
+  BASE_HOA[:third_earthers] << "Snarf"
+  BASE_HOA[:jetsons][4] = "astro"
   # Should return the array of the 'show' argument
 end
